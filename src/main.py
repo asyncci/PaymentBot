9cc50fc9a8f7808004bbfb88125bb5cbc58b7e22
asyncci <myrzabekovadik9@gmail.com>
2025-01-16 05:57:23 +0600

    logging fixed

--- src/main.py
+++ src/main.py
@@ -33,7 +33,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     if user == ADMIN_ID: 
         await admin.start(update, context)
     else:
-        logging.info(user.message.chat)
+        logging.info(f"User requested 'start' command: {update.message.chat}")
         if user in await admin.blockedUserIDs():
             text = 'Вы были заблокированы.⛔️\nЕсли у вас остались вопросы, напишите @igrokweb'
             await update.message.reply_text(text)
@@ -56,8 +56,8 @@ async def handle_reply(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
 async def button_handler(update: Update, context: CallbackContext) -> None:
     await admin.button_handler(update, context)
 
-#logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    #level=logging.DEBUG)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
 
 app.add_handler(CommandHandler('start', start));
 app.add_handler(MessageHandler(filters.ALL & ~filters.COMMAND, handle_reply))
